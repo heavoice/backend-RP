e2bd7e90e3a5af35f04fcbffbda4976cb4c11660
wapiiqo <muhammadwafiq1313@gmail.com>
2025-07-31 07:52:02 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/users", userRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Server Berjalan");
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
